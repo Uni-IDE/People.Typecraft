6e36224c58e735bcecaffa82f156ffb211920cc4
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-03-28 01:44:49 +0000

    Create dl.sh

---
+++ ark./photon/dl.sh
@@ -0,0 +1,24 @@
+d=Linux
+mkdir -p $d; cd $d
+yt-dlp https://youtu.be/cGxm4tvF5E8
+cd ..
+
+d=Neovim_Configuration
+mkdir -p $d; cd $d
+yt-dlp https://www.youtube.com/playlist?list=PLsz00TDipIffxsNXSkskknolKShdbcALR
+cd ..
+
+d=Neovim_for_Newbs_FREE_NEOVIM_COURSE
+mkdir -p $d; cd $d
+yt-dlp https://www.youtube.com/playlist?list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn 
+cd ..
+
+d=Vim_Tips_and_Tricks
+mkdir -p $d; cd $d
+yt-dlp https://www.youtube.com/playlist?list=PLsz00TDipIffY84NOkuTETHVa5FINZj5P
+cd ..
+
+d=Vim_Plugins_You_NEED_TO_KNOW
+mkdir -p $d; cd $d
+yt-dlp https://www.youtube.com/playlist?list=PLsz00TDipIfd500OiPVRyZIc9TLHRRgIM
+cd ..
